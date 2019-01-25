9da7b41e1f9e2df0ecb323f6edf1543bc27abefb
Zeke Sonxx <zeke@zekesonxx.com>
2019-01-24 23:13:20 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# archmirror
+
+A simple little set of scripts to maintain a partial Arch mirror.
+
---
+++ update.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+# archmirror v0.0.0
+# https://github.com/zekesonxx
+
+run_pacman() {
+	fakeroot pacman --config ./pacman.conf --noconfirm "$@"
+}
+
+run_powerpill() {
+	fakeroot powerpill --powerpill-config ./powerpill.json \
+		--config ./pacman.conf --noconfirm $@
+}
+
+check_cmd() {
+	if hash "$1" 2>>/dev/null; then
+		echo "$1 available";
+	else
+		echo "$1 not found, exiting";
+		exit 1
+	fi
+}
+echo "# Checking for available programs..."
+check_cmd fakeroot
+check_cmd aria2c
+check_cmd rsync
+check_cmd pacman
+check_cmd powerpill
+check_cmd paccache
+
+echo "# Generating package list..."
+for filename in ./lists/*; do
+	echo "$(basename "$filename"): $(wc -l < "$filename") packages";
+done
+package_list="$(cat ./lists/* | sort -u)"
+echo "Generated list with $(echo "$package_list" | wc -l) packages"
+
+echo "# Updating package database..."
+run_pacman -Sy
+
+echo "# Downloading updated packages"
+run_powerpill --powerpill-clean -Sw "$package_list"
+
+echo "# Pruning duplicate packages"
+paccache -c ./packages -rk 1
+
+echo "# Updated!"
